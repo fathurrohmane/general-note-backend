2492fb4ec122c467056efde12a789f3ffc1587bc
Fathurrohman Elkusnandi <felkusnandi@deloitte.com>
2025-06-09 16:08:31 +0700

    feat: Get current user in authenticated end point

--- src/main/kotlin/com/elkusnandi/generalnote/controller/NoteController.kt
+++ src/main/kotlin/com/elkusnandi/generalnote/controller/NoteController.kt
@@ -1,5 +1,7 @@
 package com.elkusnandi.generalnote.controller
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController
 class NoteController {
 
     @GetMapping()
-    fun getNotes() : String {
-        return "notes"
+    fun getNotes(@AuthenticationPrincipal userDetails: UserDetails) : String {
+        return "${userDetails.username}'s notes"
     }
 
 }
